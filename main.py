fb07e4a50a0124ccacc8ae362529d80b9133d6c6
Aw1ks <aw1ksss331@gmail.com>
2025-01-13 16:52:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,37 +8,33 @@ from telegram import Bot
 from dotenv import load_dotenv
 
 
-def get_link_of_the_random_comic_book():
-    comic_book_link = 'https://xkcd.com/info.0.json'
-    comic_book_response = requests.get(comic_book_link)
-    comic_book_response.raise_for_status()
-    comic_book_image = comic_book_response.json()
-    comic_book_number = int(comic_book_image['num'])
+def get_link_of_the_random_comic():
+    comic_link = 'https://xkcd.com/info.0.json'
+    comic_response = requests.get(comic_link)
+    comic_response.raise_for_status()
+    comic_image = comic_response.json()
+    comic_number = int(comic_image['num'])
 
-    random_comic_book_number =  random.randint(1, comic_book_number)
-    random_comic_book_link = f'https://xkcd.com/{random_comic_book_number}/info.0.json'
-    random_comic_book_response = requests.get(random_comic_book_link)
-    random_comic_book = random_comic_book_response.json()
+    random_index_comic = random.randint(1, comic_number)
+    random_comic_link = f'https://xkcd.com/{random_index_comic}/info.0.json'
+    comic_link_response = requests.get(random_comic_link)
+    comic_link_response.raise_for_status()
+    random_comic = comic_link_response.json()
 
-    return random_comic_book
+    return random_comic
 
 
-def get_image_of_the_comic(comic_link):
-    image_comic_book = comic_link['img']
-    filename = 'comic_book.png'
+def get_comic_image(comic_link):
+    comic_image = comic_link['img']
+    filename = 'comic.png'
 
-    image_comic_book_response = requests.get(image_comic_book)
-    image_comic_book_response.raise_for_status()
+    comic_image_response = requests.get(comic_image)
+    comic_image_response.raise_for_status()
 
     with open(filename, 'wb') as file:
-        file.write(image_comic_book_response.content)
+        file.write(comic_image_response.content)
 
     return filename
-
-
-def get_text_from_comic_book_image(comic_link):
-
-    return comic_link['alt']
 
 
 async def send_message(tg_bot_token, tg_chat_id, filename, text):
@@ -46,20 +42,20 @@ async def send_message(tg_bot_token, tg_chat_id, filename, text):
     await bot.send_photo(chat_id=tg_chat_id, photo=filename, caption=text)
 
 
-
 def main():
     load_dotenv()
 
-    tg_bot_token = os.getenv('TG_BOT_TOKEN')
-    tg_chat_id = os.getenv('TG_CHAT_ID')
-
-    comic_link = get_link_of_the_random_comic_book()
-    filename = get_image_of_the_comic(comic_link)
-    text = get_text_from_comic_book_image(comic_link)
+    tg_bot_token = os.environ['TG_BOT_TOKEN']
+    tg_chat_id = os.environ['TG_CHAT_ID']
 
     asyncio.run(send_message(tg_bot_token, tg_chat_id, filename, text))
 
-    os.remove(filename)
+    try:
+        comic_link = get_link_of_the_random_comic()
+        filename = get_comic_image(comic_link)
+        text = comic_link['alt']
+    finally:
+        os.remove(filename)
 
 
 if __name__ == "__main__":
